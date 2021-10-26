a3da52eb3d97c1aa5e794472ced5276c80b0548d
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-10-26 22:29:31 +0500

    conncet android emulator

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Na gap jora!!</Text>
+      <Text>Assalomu aleykum</Text>
       <StatusBar style="auto" />
     </View>
   );

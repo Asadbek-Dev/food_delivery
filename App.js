ef4a2cfaa5a920ed040287ed956e4cd70eb9210d
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-10-26 21:43:17 +0500

    added necessary files

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Na gap jora!!</Text>
       <StatusBar style="auto" />
     </View>
   );

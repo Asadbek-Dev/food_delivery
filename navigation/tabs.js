b43e45a3b474ffdb3506bba609c4deca8de5d791
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-11-03 00:17:09 +0500

    added tabs

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -8,4 +8,29 @@ import {createBottomTabNavigator,BottomTabBar} from '@react-navigation/bottom-ta
 
 import {Home} from '../screens';
 
-import {COLORS, icons} from '../constants'
+import {COLORS, icons} from '../constants';
+const Tab=createBottomTabNavigator();
+
+const Tabs=()=>{
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name='Home'
+                component={Home}
+                options={{
+                    tabBarIcon:({focused})=>(
+                        <Image
+                            source={icons.cutlery}
+                            resizeMode='contain'
+                            style={{
+                                width:25,
+                                height:25,
+                                tintColor:focused ? COLORS.primary
+                            }}
+                        />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    )
+}

e34cf2cc8c6a05099e54b8a5ece3fc429f92ac06
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-11-02 21:48:57 +0500

    added necessary pakeges

---
+++ navigation/tabs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {
+    View,
+    Input,
+    TouchableOpacity
+} from 'react-native';
+import {createBottomTabNavigator,BottomTabBar} from '@react-navigation/bottom-tabs';
+
+import {Home} from '../screens';
+
+import {COLORS, icons} from '../constants'
